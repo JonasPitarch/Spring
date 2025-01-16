2f2633c41c397490bd61e05f33dcec3617a8897a
JonasPitarch <162315778+JonasPitarch@users.noreply.github.com>
2025-01-16 10:00:56 +0100

    Add files via upload

---
+++ TablaEquipos/src/main/java/com/example/jonas/TablaEquipos/TablaEquiposApplication.java
@@ -0,0 +1,13 @@
+package com.example.jonas.TablaEquipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablaEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablaEquiposApplication.class, args);
+	}
+
+}
---
+++ TablaEquipos/src/test/java/com/example/jonas/TablaEquipos/TablaEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jonas.TablaEquipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablaEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
